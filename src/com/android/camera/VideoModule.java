21f28d8c378285f24712910c42d7bb09283caba3
Darescu Ionut <johnny@esi.ro>
2013-03-07 10:33:57 +0200

    Revert "Add storage selection (1/2)"
    This reverts commit b6518db0e4d50a8271577596ed46380211270109.
    
    Conflicts:
    	res/xml/video_preferences.xml

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -430,8 +430,6 @@ public class VideoModule implements CameraModule,
         mPrefVideoEffectDefault = mActivity.getString(R.string.pref_video_effect_default);
         resetEffect();
 
-        Storage.setStorage(CameraSettings.readStorage(mPreferences));
-
         // Power shutter
         mActivity.initPowerShutter(mPreferences);
 
@@ -1445,7 +1443,7 @@ public class VideoModule implements CameraModule,
         // Used when emailing.
         String filename = title + convertOutputFormatToFileExt(outputFileFormat);
         String mime = convertOutputFormatToMimeType(outputFileFormat);
-        String path = Storage.generateDirectory() + '/' + filename;
+        String path = Storage.DIRECTORY + '/' + filename;
         String tmpPath = path + ".tmp";
         mCurrentVideoValues = new ContentValues(7);
         mCurrentVideoValues.put(Video.Media.TITLE, title);
@@ -2342,13 +2340,6 @@ public class VideoModule implements CameraModule,
 
             // Check if the current effects selection has changed
             if (updateEffectSelection()) return;
-
-            String storage = CameraSettings.readStorage(mPreferences);
-            if (!storage.equals(Storage.getStorage())) {
-                Storage.setStorage(storage);
-                mActivity.updateStorageSpaceAndHint();
-                mActivity.reuseCameraScreenNail(!mIsVideoCaptureIntent);
-            }
 
             readVideoPreferences();
             showTimeLapseUI(mCaptureTimeLapse);
